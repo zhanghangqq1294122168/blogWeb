a5e4026beb4cae7515c97f80458f8fb27cf97151
zhangHang <1294122168@qq.com>
2018-11-04 21:59:21 +0800

    1

--- src/main/java/cn/zh/blog/controller/FileController.java
+++ src/main/java/cn/zh/blog/controller/FileController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -22,6 +23,7 @@ public class FileController {
 
 
     @PostMapping("/upload")
+    @ResponseBody
     public String upload(@RequestParam("file") MultipartFile file) {
         String url;
         if (!file.isEmpty()) {
